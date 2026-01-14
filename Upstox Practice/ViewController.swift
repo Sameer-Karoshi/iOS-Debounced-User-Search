b6f20a6ae268d29d7d56115f0cf5a9dcc4b652f4
Sameer Karoshi <sameersuhaskaroshi2000@gmail.com>
2026-01-14 19:41:56 +0530

    Initial Commit

---
+++ Upstox Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Upstox Practice
+//
+//  Created by Sameer Karoshi on 14/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
